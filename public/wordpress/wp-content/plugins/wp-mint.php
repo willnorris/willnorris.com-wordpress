b72162c24fda1e75ce942368cced94967b9dde05
Will Norris <will@willnorris.com>
2006-08-24 22:21:29 +0000

    added simple mint plugin

---
+++ public/wordpress/wp-content/plugins/wp-mint.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: WP-Mint
+Description: Inserts <a href="http://haveamint.com">Mint</a> link into page headers
+Version: 1.0
+Author: Will Norris
+Author URI: http://www.willnorris.com
+*/
+
+function insert_mint_script_tag()
+{
+	echo '<script src="/mint/?js" type="text/javascript"></script>';
+}
+
+add_action('wp_head', 'insert_mint_meta_tag');
+
+?>
