b224571978cfd0a414f1bf63bbff4160023b66d4
Will Norris <will@willnorris.com>
2006-09-02 01:18:37 +0000

    use myopenid.com for OpenID delegate

--- public/wordpress/wp-content/plugins/wp-xrds.php
+++ public/wordpress/wp-content/plugins/wp-xrds.php
@@ -11,6 +11,10 @@ Author URI: http://www.willnorris.com
 function insert_xrds_meta_tag()
 {
 	echo '<meta http-equiv="X-XRDS-Location" content="'.get_option('home').'/xrds.xml" />';
+	//echo '<link rel="openid.server" href="http://www.livejournal.com/openid/server.bml" />';
+	//echo '<link rel="openid.delegate" href="http://hugwill.livejournal.com/" />';
+	echo '<link rel="openid.server" href="https://www.myopenid.com/server" />';
+	echo '<link rel="openid.delegate" href="https://wnorris.myopenid.com/" />';
 }
 
 add_action('wp_head', 'insert_xrds_meta_tag');
