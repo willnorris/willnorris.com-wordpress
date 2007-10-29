7be677974178dc8015ac8160c8271866114f17f8
Will Norris <will@willnorris.com>
2007-10-29 08:31:27 +0000

    revert r289, but achieve similar functionality with hooks

--- public/wordpress/wp-content/themes/willnorris/functions.php
+++ public/wordpress/wp-content/themes/willnorris/functions.php
@@ -15,7 +15,34 @@ function willnorris_footer() {
 <?php
 }
 
+function willnorris_remove_title_prefix($title, $sep) {
+	error_log("$title => $sep");
+	$prefix = " $sep ";
+	if (strpos($title, $prefix) == 0) {
+		$title = substr($title, strlen($prefix));
+	}
+
+	return $title;
+}
+
+
+function willnorris_bloginfo($output, $show) {
+	global $willnorris_removed_title;
+
+	if (is_singular() && $show == 'name') {
+		if (empty($willnorris_removed_title)) {
+			$willnorris_removed_title = 1;
+			add_filter('wp_title', 'willnorris_remove_title_prefix', 5, 2);
+			return '';
+		}
+	}
+
+	return $output;
+}
+
 //add_action('wp_head', 'willnorris_header');
 add_action('get_footer', 'willnorris_footer');
+add_filter('bloginfo', 'willnorris_bloginfo', 5, 2);
 
 ?>
+
