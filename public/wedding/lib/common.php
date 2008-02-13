43cbf492c90c378e73f62f6bc0cfe7c2427bd6b4
Will Norris <will@willnorris.com>
2008-02-13 06:18:28 +0000

    ignore local configuration

--- public/wedding/lib/common.php
+++ public/wedding/lib/common.php
@@ -6,8 +6,9 @@ require_once 'Invitation.php';
 require_once 'Guest.php';
 require_once 'messages.php';
 require_once 'pre.php';
+require_once 'local.php';
 
-$db =& MDB2::factory('mysql://username:password@localhost/database');
+$db =& MDB2::factory($db_dsn);
 if (PEAR::isError($db)) {
 	die($db->getMessage());
 }
