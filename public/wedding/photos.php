aab581685cb78a3d924e8455623d9bfebbedcf5e
Will Norris <will@willnorris.com>
2008-02-19 09:16:49 +0000

    fill out photo page

--- public/wedding/photos.php
+++ public/wedding/photos.php
@@ -8,6 +8,10 @@
 
 	<h2>Photos</h2>
 
+	<h3>Engagement</h3>
+	
+	<p>In February 2008, we did engagement photos at the <a href="http://getty.edu/visit/">Getty Villa</a> and nearby beach in Malibu, CA.</p>
+
 	<ul id="thumbnails">
 		<li><a href="images/photos/001.jpg"><img src="images/photos/thumbnails/001.jpg" /></a></li>
 		<li><a href="images/photos/002.jpg"><img src="images/photos/thumbnails/002.jpg" /></a></li>
@@ -25,6 +29,14 @@
 		<li><a href="images/photos/014.jpg"><img src="images/photos/thumbnails/014.jpg" /></a></li>
 		<li><a href="images/photos/015.jpg"><img src="images/photos/thumbnails/015.jpg" /></a></li>
 	</ul>
+
+	<h3>Wedding Photos</h3>
+
+	<p>While we will have a photographer at the wedding, we'd love for family 
+and friends to take pictures as well.  If you don't mind, please upload any 
+photos to our <a href="http://flickr.com/groups/norris-wedding/">Flickr Group</a>.  Alternately, please tag any photos 
+or other media with the tag <kbd>NorrisWedding2008<kbd>.
+
 <?php
 
 	pageFooter();
