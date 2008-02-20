8bbe2352fcc1dbc4c2627d3d419e181227d213a6
Will Norris <will@willnorris.com>
2008-02-20 17:16:39 +0000

    first stab at our story and outline for front page

--- public/wedding/about.php
+++ public/wedding/about.php
@@ -9,40 +9,75 @@
 	<h2>About Us</h2>
 
 	<div id="our-story">
-		<p class="will">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-	Integer mauris sem, vehicula auctor, tempus nec, bibendum vel, risus. Sed 
-	tincidunt elit non enim. Donec libero nisi, ullamcorper at, aliquam ac, 
-	interdum in, quam. Sed ante nulla, rhoncus nec, elementum eu, pulvinar non, 
-	velit. Curabitur malesuada. Nam porta tincidunt pede. In lobortis fermentum 
-	orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per 
-	inceptos hymenaeos. Nam purus elit, euismod nec, porta sed, placerat non, 
-	felis. Donec hendrerit elit a lectus.</p>
 
-		<p class="elisabeth">Lorem ipsum dolor sit amet, elit a lectus.</p>
+<p class="will">I guess our story starts in 2001 while I was at college in 
+Atlanta, GA.  I was studying computer science and had been a pretty big fan of 
+the rock band Skillet for about a year, so I decided to setup a website about 
+the band.  The site quickly grew and evolved into a decent size community of 
+Skillet fans.</p>
 
-		<p class="elisabeth">Lorem ipsum dolor sit amet, elit a lectus.  
-Tincidunt elit non enim. Donec libero nisi, ullamcorper at, aliquam ac, 
-interdum in, quam. Sed ante nulla, rhoncus nec, elementum eu, pulvinar non, 
-velit. Curabitur malesuada. Nam porta tincidunt pede. In lobortis fermentum 
-</p>
+<p class="elisabeth">I was here in Southern California, and had been introduced 
+to Skillet that same year by my best friend.  I was aware of the community Will 
+had setup, but didn't really like a lot of the people there.  So I got involved 
+with, and later managed, a different Skillet community.  Of course Will and I 
+knew who each other were and he helped me on a few technical things, but 
+otherwise we kept to ourselves.  Honestly, he tended to come off as a bit of an 
+elitist snob.</p>
 
-		<p class="will">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-	Integer mauris sem, vehicula auctor, tempus nec, bibendum vel, risus. Sed 
-	tincidunt elit non enim. Donec libero nisi, ullamcorper at, aliquam ac, 
-	interdum in, quam. Sed ante nulla, rhoncus nec, elementum eu, pulvinar non, 
-	velit. Curabitur malesuada. Nam porta tincidunt pede. In lobortis fermentum 
-	orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per 
-	inceptos hymenaeos. Nam purus elit, euismod nec, porta sed, placerat non, 
-	felis. Donec hendrerit elit a lectus.</p>
-		<p class="elisabeth">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
-	Integer mauris sem, vehicula auctor, tempus nec, bibendum vel, risus. Sed 
-	tincidunt elit non enim. Donec libero nisi, ullamcorper at, aliquam ac, 
-	interdum in, quam. Sed ante nulla, rhoncus nec, elementum eu, pulvinar non, 
-	velit. Curabitur malesuada. Nam porta tincidunt pede. In lobortis fermentum 
-	orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per 
-	inceptos hymenaeos. Nam purus elit, euismod nec, porta sed, placerat non, 
-	felis. Donec hendrerit elit a lectus.</p>
-	</div>
+<p class="will">Yeah? Well you seemed to be running a preschool with little 
+kids just running amuck!</p>
+
+<p class="elisabeth">Anyway, needless to say we didn't exactly get along.  We 
+had two very different communities that attracted very different types of 
+people, and we generally left it at that.</p>
+
+<p class="will">Now by the summer of 2002, I had moved back home to Memphis, 
+Tennessee.  I wasn't working that summer so a few friends from the Skillet 
+community and I decided to take a road trip to California.  Skillet was playing 
+several shows over a couple of days, so we arranged for a dozen or so of us 
+that knew each other online to all meet up at the shows.  There's several folks 
+missing from this picture, but you can see Elisabeth and I kneeling down in 
+front <a href="">here</a>.</p>
+
+<p class="elisabeth">Those concerts that summer changed a lot for me.  Will was 
+always a bit intimidating to a lot of people, because he ran the big community 
+and knew the band pretty well and all.  I didn't really know what to expect 
+when I met him, but I was pretty much ready to not like him much.  What I saw 
+though was this skinny guy with firey red hair and moccasins... and he was 
+actually kinda cute.  Without realizing it, I basically fell in love with my 
+arch-enemy.</p>
+
+<p class="elisabeth">But then of course he went back home to Memphis.  I tried 
+to pursue him a bit, but he wouldn't give me the time of day.</p>
+
+<p class="will">What?  When did you pursue me?  The fact of the matter is, I 
+thought she was pretty cute.  I mean really, did you see that picture up there?  
+But I was also realistic... we lived almost 2000 miles apart, hardly knew each 
+other, and weren't really in the position to get into a long distance 
+relationship.  She was a beautiful girl, but that was it.</p>
+
+<p class="elisabeth">About a year later in 2003, the two communities that we 
+were managing merged into one.  Will and I worked together a bit to pull that 
+off and then co-managed the new community for a while.  By this time though, I 
+had begun working with a different band and ultimately left the Skillet 
+community to go work with them, leaving things to Will.  Except for a couple of 
+short emails, we didn't really talk much for the next 2 or 3 years... we had 
+gone our separate ways.</p>
+
+<p class="will">During those next few years I had been going to school and 
+working at the University of Memphis.  I was working on a pretty cool project 
+called Shibboleth that was used by a lot universities around the world.  In 
+October 2005, I heard that a school in California was looking for a Shibboleth 
+administrator, so I expressed my interest in the position.  My job at UofM was 
+only a part-time position and I had no real ties to Memphis, so I figured I had 
+nothing to lose.</p>
+
+<p class="will">I got in touch with a couple of friends I had in California 
+just to let them know I was looking at a job out there.  I remembered that 
+Elisabeth was in school, so I contact her to find out where and to see what she 
+had been up to the last couple of years.</p>
+
+<p>... still writing the rest. :-\</p>
 
 <?php
 	pageFooter();
--- public/wedding/index.php
+++ public/wedding/index.php
@@ -4,7 +4,13 @@
 	pageHeader();
 ?>
 
-	<h2>Index</h2>
+	<ul id="toc">
+		<li id="toc-about"><a href="about">About Us</a></li>
+		<li id="toc-rsvp"><a href="rsvp">RSVP</a></li>
+		<li id="toc-info"><a href="info">Wedding Info</a></li>
+		<li id="toc-photos"><a href="photos">Photos</a></li>
+		<li id="toc-contact"><a href="contact">Contact Us</a></li>
+	</ul>
 
 <?php
 
