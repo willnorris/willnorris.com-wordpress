f1b245fcacbb7f8d8f7f133dc8f9ec2c0bd467bb
Will Norris <will@willnorris.com>
2009-05-09 00:57:09 +0000

    update bkite-profile plugin

--- public/wordpress-content/plugins/bkite-profile.php
+++ public/wordpress-content/plugins/bkite-profile.php
@@ -19,7 +19,7 @@ function brightkite_profile($userid) {
 	$sql = $wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'actionstream_items WHERE user_id=%s AND service=%s AND setup_idx=%s ORDER BY created_on DESC', $userid, 'brightkite', 'checkins');
 	$last_checkin = $wpdb->get_row($sql);
 
-	if (true || empty($loc) || $last_checkin->identifier_hash != $loc['hash']) {
+	if (empty($loc) || $last_checkin->identifier_hash != $loc['hash']) {
 
 		$checkin_data = unserialize($last_checkin->data);
 		$checkin_xml = simplexml_load_file($checkin_data['url'] . '.xml');
@@ -29,6 +29,7 @@ function brightkite_profile($userid) {
 				'hash' => $last_checkin->identifier_hash,
 				'name' => "{$checkin_xml->place->name}",
 				'url' => $checkin_data['url'],
+				'timestamp' => (int) $checkin_xml->created_at_ts,
 				'lat' => "{$checkin_xml->place->latitude}",
 				'lon' => "{$checkin_xml->place->longitude}",
 			);
@@ -41,8 +42,8 @@ function brightkite_profile($userid) {
 	if ($loc) {
 ?>
 	<dl class="geo">
-		<dt><abbr class="latitude" title="<?php _e($loc['lat']) ?>">Current</abbr> <abbr class="longitude" title="<?php _e($loc['lon']) ?>">Location</abbr>:</dt>
-		<dd><a href="<?php _e($loc['url']) ?>"><?php _e($loc['name']) ?></a></dd>
+		<dt><abbr class="latitude" title="<?php _e($loc['lat']) ?>">Last</abbr> <abbr class="longitude" title="<?php _e($loc['lon']) ?>">Seen</abbr>:</dt>
+		<dd><a href="<?php _e($loc['url']) ?>"><?php _e($loc['name']) ?></a><br /> <em>(<?php echo human_time_diff($loc['timestamp'], time()); ?> ago)</em></dd>
 	</dl>
 <?php
 	}
