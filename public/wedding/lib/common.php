2deab0cbf77c295e293e10d0213a98bce42fdcac
Will Norris <will@willnorris.com>
2009-06-24 00:22:40 +0000

    remove MDB2 dependency

--- public/wedding/lib/common.php
+++ public/wedding/lib/common.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'MDB2.php';
+//require_once 'MDB2.php';
 require_once 'functions.php';
 require_once 'Invitation.php';
 require_once 'Guest.php';
@@ -8,10 +8,12 @@ require_once 'messages.php';
 require_once 'pre.php';
 require_once 'local.php';
 
+/*
 $db =& MDB2::factory($db_dsn);
 if (PEAR::isError($db)) {
 	die($db->getMessage());
 }
+ */
 
 
 $alertEmail = 'wedding@willnorris.com';
