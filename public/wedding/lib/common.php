271a411c900c9bc6d6caeabba0f9aaab146bbb78
Will Norris <will@willnorris.com>
2009-06-24 00:26:01 +0000

    fix include path

--- public/wedding/lib/common.php
+++ public/wedding/lib/common.php
@@ -1,12 +1,12 @@
 <?php
 
 //require_once 'MDB2.php';
-require_once 'functions.php';
-require_once 'Invitation.php';
-require_once 'Guest.php';
-require_once 'messages.php';
-require_once 'pre.php';
-require_once 'local.php';
+require_once dirname(__FILE__) . '/functions.php';
+require_once dirname(__FILE__) . '/Invitation.php';
+require_once dirname(__FILE__) . '/Guest.php';
+require_once dirname(__FILE__) . '/messages.php';
+require_once dirname(__FILE__) . '/pre.php';
+require_once dirname(__FILE__) . '/local.php';
 
 /*
 $db =& MDB2::factory($db_dsn);
