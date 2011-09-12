4b81f97921ce65c6cd9439ec66b42b19e2626f23
Will Norris <will@willnorris.com>
2011-09-11 22:00:28 -0700

    no more comments

---
+++ public/wordpress-content/themes/willnorris/comments.php
@@ -0,0 +1,14 @@
+<?php
+  $googleplus_url = get_post_meta($post->ID, '_googleplus_url', true);
+  if ($googleplus_url) {
+?>
+  <p>
+    I no longer have comments on my blog.  Instead, I encourage you to 
+    <a href="<?php echo $googleplus_url; ?>">comment here on Google+</a>.  
+    If you don't have a Google+ account and would like one, just let me know.
+    Of course, you're also always welcome to post a reply on your own blog, 
+    on twitter, or wherever you choose to publish.
+  </p>
+<?php
+  }
+?>
