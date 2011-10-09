50b4d40aab6e5101c8ae5d8daad5b87ad5f09b83
Will Norris <will@willnorris.com>
2011-10-08 23:45:34 -0700

    move src to scss, add .showgrid,  update site-description styling

--- public/wordpress-content/themes/wn2011/config.rb
+++ public/wordpress-content/themes/wn2011/config.rb
@@ -2,7 +2,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/wordpress-content/themes/wn2011/"
 css_dir = "css"
-sass_dir = "src"
+sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
 relative_assets = true
