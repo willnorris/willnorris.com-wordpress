0f34309202fc4ba5e88cc870750fff1053109837
Will Norris <will@willnorris.com>
2011-12-31 19:46:49 +0000

    add hum support for /c/

--- public/wordpress-content/mu-plugins/willnorris.php
+++ public/wordpress-content/mu-plugins/willnorris.php
@@ -74,5 +74,6 @@ add_filter('wp_redirect_status', create_function('$s', 'status_header($s); retur
 
 // Hum Extensions
 add_filter('hum_shortlink_base', create_function('', 'return "http://wjn.me/";'));
+add_filter('hum_redirect_base_c', create_function('', 'return "http://code.willnorris.com/";'));
 add_filter('hum_redirect_base_w', create_function('', 'return "http://wiki.willnorris.com/";'));
 add_filter('amazon_affiliate_id', create_function('', 'return "willnorris-20";'));
