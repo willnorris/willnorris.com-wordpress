61e2be7a5a276a80dc7ec3e66e634df2682a518c
Will Norris <will@willnorris.com>
2012-06-13 13:10:09 -0700

    cleanup single post page

--- public/content/themes/wn2011/comments.php
+++ public/content/themes/wn2011/comments.php
@@ -4,15 +4,10 @@
   if ($post->post_type == 'post' && $googleplus_url) {
 ?>
   <hr />
-  <h4><a href="<?php echo $googleplus_url; ?>">Discuss on Google+</a></h4>
-
   <p>
     I no longer have comments on my blog.  Instead, I encourage you to
-    <a href="<?php echo $googleplus_url; ?>">comment here on Google+</a>.
-    If you don't have a Google+ account, you can sign up
-    <a href="http://www.google.com/+">here</a>.  Of course, you're also always
-    welcome to post a reply on your own blog, on twitter, or wherever you
-    choose to publish.
+    <a href="<?php echo $googleplus_url; ?>">comment here on Google+</a>, or
+    even better, continue the discussion on your own blog.
   </p>
 <?php
   }
