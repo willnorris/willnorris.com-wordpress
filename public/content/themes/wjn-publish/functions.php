be1b6139c3ac4b95d6c1aa12844f55cd4ba8c273
Will Norris <will@willnorris.com>
2013-08-09 15:29:57 -0700

    override publish_posted_on

--- public/content/themes/wjn-publish/functions.php
+++ public/content/themes/wjn-publish/functions.php
@@ -32,3 +32,16 @@ class WJN_Publish {
 }
 
 new WJN_Publish;
+
+function publish_posted_on() {
+  printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date dt-published" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard h-card p-author">%8$s<a class="u-url p-name" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'publish' ) . '.',
+    esc_url( get_permalink() ),
+    esc_attr( get_the_time() ),
+    esc_attr( get_the_date( 'c' ) ),
+    esc_html( get_the_date() ),
+    esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
+    esc_attr( sprintf( __( 'View all posts by %s', 'publish' ), get_the_author() ) ),
+    esc_html( get_the_author() ),
+    get_avatar( get_the_author_meta( 'ID' ) )
+  );
+}
