ff593f5ecabc8f093b480b2f51ef1d64f936d0c1
Will Norris <will@willnorris.com>
2014-01-05 18:43:11 -0800

    fix twitter card metadata

--- public/content/mu-plugins/twitter-cards.php
+++ public/content/mu-plugins/twitter-cards.php
@@ -1,12 +1,14 @@
 <?php
 
 function twitter_metadata($metadata) {
+  $metadata['twitter:card'] = 'summary';
+  $metadata['twitter:creator'] = '@willnorris';
+
   foreach (array('url', 'title', 'description', 'image') as $attr) {
     if (array_key_exists("og:$attr", $metadata)) {
       $metadata["twitter:$attr"] = $metadata["og:$attr"];
     }
   }
-  $metadata['twitter:creator'] = '@willnorris';
 
   return $metadata;
 }
