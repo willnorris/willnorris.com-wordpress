8de53123f5c031b61046f6f4fa7955afbcfb4250
Will Norris <will@willnorris.com>
2014-02-17 14:57:01 -0800

    update debug mu-plugin
    - simplify parsing of /.info requests
    - add debug_pre and debug_die helper functions

--- public/content/mu-plugins/debug.php
+++ public/content/mu-plugins/debug.php
@@ -20,40 +20,25 @@ add_action('deprecated_file_included', 'debug_deprecated_backtrace');
 add_action('deprecated_argument_run', 'debug_deprecated_backtrace');
 add_action('doing_it_wrong_run', 'debug_deprecated_backtrace');
 
-
-/**
- * Accept debug query variables.
- */
-function debug_query_vars( $vars ) {
-  $vars[] = 'phpinfo';
-  return $vars;
-}
-add_action('query_vars', 'debug_query_vars');
-
-
 /**
  * Display PHP info when requested.  Only displayed to logged in admins.
  */
-function debug_phpinfo_parse_request( $wp ) {
-  if ( array_key_exists( 'phpinfo', $wp->query_vars ) ) {
-    if ( is_user_logged_in() && current_user_can('manage_options') ) {
-      phpinfo();
-      exit;
-    }
+add_action('parse_request', function( $wp ) {
+  if ( $wp->request == '.info' && current_user_can('manage_options') ) {
+    phpinfo();
+    exit;
   }
+});
+
+function debug_pre($arg, $print = false) {
+  $str = '<pre>' . print_r($arg, true) . '</pre>';
+  return $print ? print($str) : $str;
 }
-add_action('parse_request', 'debug_phpinfo_parse_request');
 
-
-/**
- * Add rewrite rules for debug features.
- */
-function debug_rewrite_rules( $wp_rewrite ) {
-  $debug_rules = array(
-    '.info' => 'index.php?phpinfo=true',
-  );
-
-  $wp_rewrite->rules = $debug_rules + $wp_rewrite->rules;
+function debug_die() {
+  $str = '';
+  foreach( func_get_args() as $arg ) {
+    $str .= debug_pre($arg);
+  }
+  wp_die($str);
 }
-add_action('generate_rewrite_rules', 'debug_rewrite_rules');
-
