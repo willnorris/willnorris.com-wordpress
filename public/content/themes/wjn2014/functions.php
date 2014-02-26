5099d0c169602c268cc1885a3989c84dcf20057c
Will Norris <will@willnorris.com>
2014-02-25 16:47:44 -0800

    tabs to spaces

--- public/content/themes/wjn2014/functions.php
+++ public/content/themes/wjn2014/functions.php
@@ -9,68 +9,68 @@ define( 'CHILD_THEME_VERSION', '0.1.0' );
 
 add_action( 'after_setup_theme', 'wjn2014_theme_setup' );
 function wjn2014_theme_setup() {
-	// Add HTML5 markup structure
-	add_theme_support( 'html5' );
+  // Add HTML5 markup structure
+  add_theme_support( 'html5' );
 
-	// Add viewport meta tag for mobile browsers
-	add_theme_support( 'genesis-responsive-viewport' );
+  // Add viewport meta tag for mobile browsers
+  add_theme_support( 'genesis-responsive-viewport' );
 
-	//* Add support for structural wraps
-	add_theme_support( 'genesis-structural-wraps', array(
-		'header',
-		'nav',
-		'subnav',
-		'site-inner',
-		'footer-widgets',
-		'footer',
-	) );
+  //* Add support for structural wraps
+  add_theme_support( 'genesis-structural-wraps', array(
+    'header',
+    'nav',
+    'subnav',
+    'site-inner',
+    'footer-widgets',
+    'footer',
+  ) );
 
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'link',
-		'gallery',
-		'status',
-		'quote',
-		'image',
-		'video'
-	) );
+  add_theme_support( 'post-formats', array(
+    'aside',
+    'link',
+    'gallery',
+    'status',
+    'quote',
+    'image',
+    'video'
+  ) );
 
-	// Unregister layout settings
-	genesis_unregister_layout( 'content-sidebar-sidebar' );
-	genesis_unregister_layout( 'sidebar-content-sidebar' );
-	genesis_unregister_layout( 'sidebar-sidebar-content' );
+  // Unregister layout settings
+  genesis_unregister_layout( 'content-sidebar-sidebar' );
+  genesis_unregister_layout( 'sidebar-content-sidebar' );
+  genesis_unregister_layout( 'sidebar-sidebar-content' );
 
-	// Unregister secondary sidebar
-	unregister_sidebar( 'sidebar-alt' );
+  // Unregister secondary sidebar
+  unregister_sidebar( 'sidebar-alt' );
 
-	// Remove Edit link
-	add_filter( 'genesis_edit_post_link', '__return_false' );
+  // Remove Edit link
+  add_filter( 'genesis_edit_post_link', '__return_false' );
 
-	// use author link instead of author posts link
-	add_filter( 'genesis_post_info', function( $info) {
-		return '[post_date] ' . __( 'by', 'genesis' ) . ' [post_author_link] [post_comments] [post_edit]';
-	});
+  // use author link instead of author posts link
+  add_filter( 'genesis_post_info', function( $info) {
+    return '[post_date] ' . __( 'by', 'genesis' ) . ' [post_author_link] [post_comments] [post_edit]';
+  });
 
-	add_filter( 'comment_author_says_text', '__return_empty_string');
+  add_filter( 'comment_author_says_text', '__return_empty_string');
 
-	// remove footer content
-	remove_action( 'genesis_footer', 'genesis_do_footer' );
+  // remove footer content
+  remove_action( 'genesis_footer', 'genesis_do_footer' );
 }
 
 // add footer widgets
 add_action( 'after_setup_theme', function() {
-	// Add support for 3-column footer widgets
-	add_theme_support( 'genesis-footer-widgets', 3 );
+  // Add support for 3-column footer widgets
+  add_theme_support( 'genesis-footer-widgets', 3 );
 }, 5);
 
 
 // Return {theme_dir}/css/style.css as the stylesheet_uri.
 add_filter( 'stylesheet_uri', function() {
-	return get_stylesheet_directory_uri() . '/css/style.css';
+  return get_stylesheet_directory_uri() . '/css/style.css';
 });
 
 add_filter( 'genesis_footer_creds_text', function( $text ) {
-	return '[footer_copyright] Will Norris.  Powered by [footer_wordpress_link].';
+  return '[footer_copyright] Will Norris.  Powered by [footer_wordpress_link].';
 });
 
 add_filter( 'wp_head', function() { ?>
