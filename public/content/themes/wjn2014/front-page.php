403d63944bae71190d29e7f653be7a48007f9cc2
Will Norris <will@willnorris.com>
2014-02-26 08:34:19 -0800

    update logo location

--- public/content/themes/wjn2014/front-page.php
+++ public/content/themes/wjn2014/front-page.php
@@ -4,7 +4,7 @@ add_action( 'genesis_after_header', function() {
 ?>
   <div class="home-top p-author h-card">
     <div class="wrap">
-      <img class="u-photo alignleft" src="/images/logo.jpg" alt="" />
+      <img class="u-photo alignleft" src="/logo.jpg" alt="" />
       <p><?php echo get_user_meta(2, 'description', true); ?></p>
       <?php wp_nav_menu('theme_location=social'); ?>
     </div>
